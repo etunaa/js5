27d1053eeedbab0af04973a5f15a6cc67630d2f5
etunaa <104025100+etunaa@users.noreply.github.com>
2022-06-07 20:01:27 +0400

    2

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var fruit1=["ვაშლი", "მსხალი", "ატამი"];
-function reverseArrayInPlace(fruit1){
-for (var i=0; i<=Math.floor((fruit1.length-1)/2);
+for (let i=0; i<Math.floor(fruit1.length/2);
 i++){
-    let el=fruit1[i];
-fruit1[i]=fruit1[fruit1.length-1-i];
-fruit1[fruit1.length-1-i]=el;
+     [fruit1[i], fruit1[fruit1.length-1-i]]=[fruit1[fruit1.length-1-i], fruit1[i]];
 }
-return fruit1;
-}
+
+document.write(fruit1)
